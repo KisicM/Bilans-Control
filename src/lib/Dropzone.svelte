483fdd7cab2f9b54aab5d82d414fee1cab1d7326
mihajlo.kisic <mkisic99@gmail.com>
2023-12-12 15:49:08 +0100

    init svelte app

---
+++ src/lib/Dropzone.svelte
@@ -0,0 +1,135 @@
+<!-- Dropzone.svelte -->
+
+<script lang="ts">
+  import { onMount } from 'svelte';
+  import * as XLSX from 'xlsx';
+
+  let files: File[] = [];
+  let allowedFileTypes = ['.xls', '.xlsx'];
+  let isActive: boolean = false;
+
+  function handleDrop(event: DragEvent) {
+    event.preventDefault();
+    if (event.dataTransfer == null) return
+    const newFiles = Array.from(event.dataTransfer.files) as File[];
+
+    // Remove existing files with the same name
+    files = files.filter(existingFile => !newFiles.some(newFile => newFile.name === existingFile.name));
+
+    // Add the new files
+    files = [...files, ...newFiles.filter(file => isAllowedFileType(file))];
+
+    console.log(files); // You can process the files here
+  }
+
+  function handleDragOver(event: DragEvent) {
+    event.preventDefault();
+    isActive = true;
+  }
+
+  function handleDragLeave() {
+    isActive = false;
+  }
+
+  function isAllowedFileType(file: File): boolean {
+    const fileType = file.name.slice(((file.name.lastIndexOf(".") - 1) >>> 0) + 2).toLowerCase();
+    return allowedFileTypes.includes(`.${fileType}`);
+  }
+
+  async function processFiles() {
+    for (const file of files) {
+      const workbook = await readExcelFile(file);
+      const firstRow = getFirstRow(workbook);
+      console.log(`First row of ${file.name}:`, firstRow[0]);
+      console.log(`Second row of ${file.name}:`, firstRow[1]);
+      // You can perform further processing with the first row here
+    }
+  }
+
+  async function readExcelFile(file: File): Promise<XLSX.WorkBook> {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onload = (e: ProgressEvent<FileReader>) => {
+        try {
+          if (e.target == null) return
+          const data = new Uint8Array(e.target.result as ArrayBuffer);
+          const workbook = XLSX.read(data, { type: 'array' });
+          resolve(workbook);
+        } catch (error) {
+          reject(error);
+        }
+      };
+      reader.readAsArrayBuffer(file);
+    });
+  }
+
+  function getFirstRow(workbook: XLSX.WorkBook): any[] {
+    const firstSheetName = workbook.SheetNames[0];
+    const worksheet = workbook.Sheets[firstSheetName];
+    return XLSX.utils.sheet_to_json(worksheet) as any;
+  }
+</script>
+
+<style>
+  .dropzone {
+    border: 2px dashed #3498db;
+    padding: 20px;
+    text-align: center;
+    color: #3498db;
+    transition: border 0.3s ease-in-out;
+    border-color: #2ecc71;
+  }
+
+  .file-list {
+    margin-top: 10px;
+    color: #333;
+  }
+
+  ul {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+  }
+
+  li {
+    margin-bottom: 5px;
+    font-weight: bold;
+    color: #2ecc71;
+  }
+
+  .process-button {
+    margin-top: 20px;
+    padding: 10px;
+    background-color: #2ecc71;
+    color: #fff;
+    border: none;
+    cursor: pointer;
+    transition: background-color 0.3s ease-in-out;
+  }
+
+  .process-button:hover {
+    background-color: #27ae60;
+  }
+</style>
+
+<div
+  class="dropzone"
+  on:drop="{handleDrop}"
+  on:dragover="{handleDragOver}"
+  on:dragleave="{handleDragLeave}"
+  role="region"
+  aria-label="File Drop Zone"
+>
+  <p>Drop Excel files (.xls or .xlsx) here</p>
+  {#if files.length > 0}
+    <div class="file-list">
+      <p>Added Files:</p>
+      <ul>
+        {#each files as file (file.name)}
+          <li>{file.name}</li>
+        {/each}
+      </ul>
+    </div>
+    <button on:click="{processFiles}" class="process-button">Process Files</button>
+  {/if}
+</div>
